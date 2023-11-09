81c6657b6272f973fdd52e25e9b869ea26c8028a
jamornP <100953989+jamornP@users.noreply.github.com>
2023-11-09 16:53:32 +0700

    google anlsy

--- backend/components/script.php
+++ backend/components/script.php
@@ -240,3 +240,12 @@
     bsCustomFileInput.init();
   });
 </script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-E5WX43T59K"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-E5WX43T59K');
+</script>
